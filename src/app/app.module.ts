8803ec36c15c6cc7a942e56c376333c0492969c8
anatMo <1.9.9.4anat@gmail.com>
2024-02-20 00:10:34 +0200

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ProductsListComponent } from './components/products/products-list/products-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/components/products/products-list/products-list.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../models/product.model';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrl: './products-list.component.css'
+})
+export class ProductsListComponent {
+
+  products: Product[] = [];
+  productNameFilter: string = '';
+  categoryFilter: string = '';
+  productNameFilterEnabled: boolean = false;
+  categoryFilterEnabled: boolean = false;
+  selectedFilter: string = '';
+
+  // products: Product[] = [
+  //   {
+  //     productId: '00000000-0000-0000-0000-000000000000',
+  //     productName: '6',
+  //     category: 'IPhone',
+  //     price: 4000,
+  //     unitsInStock: 20
+  //   },
+  //   {
+  //     productId: '10000000-0000-0000-0000-000000000000',
+  //     productName: '12',
+  //     category: 'IPhone',
+  //     price: 6000,
+  //     unitsInStock: 44
+  //   },
+  //   {
+  //     productId: '20000000-0000-0000-0000-000000000000',
+  //     productName: 'ES',
+  //     category: 'Galaxy',
+  //     price: 3300,
+  //     unitsInStock: 26
+  //   }
+  // ];
+
+  // constructor(private productsService: ProductsService) {}
+
+  // ngOnInit(): void {
+  //   this.loadProducts();
+  // }
+
+  // loadProducts(): void {
+  //   this.productsService.getAllProducts()
+  //     .subscribe({
+  //       next: (products) => {
+  //         this.products = products;
+  //       },
+  //       error: (response) => {
+  //         console.log(response);
+  //       }
+  //     });
+  // }
+
+  // onFilterChange(): void {
+  //   if (this.selectedFilter === 'productName') {
+  //       this.productNameFilterEnabled = true;
+  //       this.categoryFilterEnabled = false;
+  //   } else if (this.selectedFilter === 'category') {
+  //       this.productNameFilterEnabled = false;
+  //       this.categoryFilterEnabled = true;
+  //   }
+  // }
+
+
+  // get filteredProducts(): Product[] {
+  //   if (!this.products) return [];
+
+  //   if (this.productNameFilterEnabled) {
+  //     return this.products.filter(product =>
+  //       product.productName.toLowerCase().includes(this.productNameFilter.toLowerCase())
+  //     );
+  //   } else if (this.categoryFilterEnabled) {
+  //     return this.products.filter(product =>
+  //       product.category.toLowerCase().includes(this.categoryFilter.toLowerCase())
+  //     );
+  //   } else {
+  //     return [];
+  //   }
+  // }
+}
